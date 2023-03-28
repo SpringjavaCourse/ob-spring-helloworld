4fbd0980737b28475758fe9f7e7d3c45db702de2
duvan1026 <duvancastro1026@gmail.com>
2023-03-28 08:28:19 -0500

    implement scope Prototype in beans

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -23,10 +23,18 @@ public class Main {
         System.out.println(texto);
 
 
-        // EJEMPLO 2
+        // EJEMPLO 2: cargar un bean dentro de un bean
         GestorFacturas gestor = (GestorFacturas) context.getBean("gestorFacturas");
         System.out.println(gestor.calculadora.holaMundo());
 
+
+        // Concepto 3: scope o alcance
+        // Los beans por defecto son Singleton, se crea el objeto y se reutiliza para toda la aplciación
+        // podemos cambiarlo a scope="prototype" si queremos que se cree un nuevo objeto cada vez
+        // verificarlo viendo como se ejecuta varias veces un constructor
+
+//        Coche coche = new Coche(1L, "nissan", "GTR", 400, 4.5);
+//        Coche coche2 = new Coche(1L, "volkswagen", "Polo", 400, 4.5);
 
 
 
