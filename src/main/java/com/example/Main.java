d8795ea01df528bb5a3059e0df71f286a1c6e373
duvan1026 <duvancastro1026@gmail.com>
2023-03-28 07:40:29 -0500

    implement ref for apply others xml

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -13,11 +13,19 @@ public class Main {
         Calculadora service = new Calculadora();
 
         // Opcion 2. Recibir  un objeto de Spring
-        Calculadora calculadora = (Calculadora) context.getBean("calculatorService");
-
-
+        Calculadora calculadora = (Calculadora) context.getBean("calculadora");
         String texto = calculadora.holaMundo();
         System.out.println(texto);
+        // Se puede volver a obtener
+        // OJO: se recupera el mismo objeto
+        Calculadora calculadora2 = (Calculadora) context.getBean("calculadora");
+        texto = calculadora.holaMundo();
+        System.out.println(texto);
+
+
+        // EJEMPLO 2
+        GestorFacturas gestor = (GestorFacturas) context.getBean("gestorFacturas");
+        System.out.println(gestor.calculadora.holaMundo());
 
 
 
